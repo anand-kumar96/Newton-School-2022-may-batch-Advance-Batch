b135fa62f371eb1d714dd5e978f7d1295a5d26ad
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-09-08 21:20:01 +0530

    Update 110. Balanced Binary Tree.java

--- Tree/BinaryTree/Questions/TopicRelated/110. Balanced Binary Tree.java
+++ Tree/BinaryTree/Questions/TopicRelated/110. Balanced Binary Tree.java
@@ -2,55 +2,46 @@
 Leetcode Link: https://leetcode.com/problems/balanced-binary-tree/
  */
 
-package Tree.BinaryTree.Questions.Leetcode;
-
-public class BalancedBinaryTree {
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-    
-    private int isBalancedHelper(TreeNode currentNode) {
-        if(currentNode == null) {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int height(TreeNode root){
+        if(root==null){
             return 0;
         }
-
-        int leftHeight = isBalancedHelper(currentNode.left);
-        if(leftHeight == -1) {
+        int lheight=height(root.left);
+        if(lheight==-1){
             return -1;
         }
-
-        int rightHeight = isBalancedHelper(currentNode.right);
-        if(rightHeight == -1) {
+       int  rheight=height(root.right);
+        if(rheight==-1){
             return -1;
         }
-
-        if(Math.abs(leftHeight - rightHeight) > 1) {
+         if(Math.abs(lheight-rheight)>1){
             return -1;
-        }
-
-        return (Math.max(leftHeight, rightHeight) + 1);
+         }
+        return Math.max(lheight,rheight)+1;
     }
-
-    public boolean isBalanced(TreeNode root) {
-        int result = isBalancedHelper(root);
-
-        if(result != -1) {
-            return true;
-        }
-        else {
+    public boolean isBalanced(TreeNode root){
+        int ans=height(root);
+        if(ans==-1){
             return false;
         }
-
-        /*
+    return true;
+       /*
         The above code can be reduced to just one line:
         return (isBalancedHelper(root) != -1);
          */
