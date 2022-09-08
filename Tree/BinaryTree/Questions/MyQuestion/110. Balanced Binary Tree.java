7d4a46117bc70214a40010617b21d8cd26b409f6
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-09-08 21:22:13 +0530

    Create 110. Balanced Binary Tree.java

---
+++ Tree/BinaryTree/Questions/MyQuestion/110. Balanced Binary Tree.java
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/balanced-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int lheight=height(root.left);
+        if(lheight==-1){
+            return -1;
+        }
+       int  rheight=height(root.right);
+        if(rheight==-1){
+            return -1;
+        }
+         if(Math.abs(lheight-rheight)>1){
+            return -1;
+         }
+        return Math.max(lheight,rheight)+1;
+    }
+    public boolean isBalanced(TreeNode root){
+        int ans=height(root);
+        if(ans==-1){
+            return false;
+        }
+    return true;
+    }
+}
