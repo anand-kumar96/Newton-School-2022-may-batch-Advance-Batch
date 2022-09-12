068cc98e01f66c33db16d3aff0ed0d4ebf5b3b03
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-09-12 09:13:41 +0530

    Create 236. Lowest Common Ancestor of a Binary Tree.java

---
+++ Tree/BinaryTree/Questions/MyQuestion/236. Lowest Common Ancestor of a Binary Tree.java
@@ -0,0 +1,96 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null || root==p||root==q){
+            return root;
+        }
+        TreeNode left=lowestCommonAncestor(root.left, p,q);
+        TreeNode right=lowestCommonAncestor(root.right, p,q);
+        if(left==null){
+            return right;
+        }else if(right==null){
+            return left;
+        }else
+        return root;
+    }
+}
+/*
+explanation:
+3,5,1,6,2,0,8,null,null,7,4     case (1) p=7, q =4 
+                                case (2) p=6, q =8
+                                 tree
+                                 3
+                            5          1
+                         6     2    0     8
+                             7   4
+                             
+                  case 01
+         lowestCommonAncestor(root,7, 4) {
+        if(root==null || root==7||root==4){
+            return root;
+        }
+        TreeNode left=lowestCommonAncestor(root.left (5), 7,4);--------->
+        TreeNode right=lowestCommonAncestor(root.right, 7,4);
+        if(left==null){
+            return right;
+        }else if(right==null){
+            return left;
+        }else
+        return root;
+    }                
+                                                                                               lowestCommonAncestor(root(5),7, 4) {
+                                                                                              if(root==null || root==7||root==4){
+                                                                                               return root;
+                                                                                               }
+                                                                                  <--------   TreeNode left=lowestCommonAncestor(root.left (6), 7,4);
+                                                                                              TreeNode right=lowestCommonAncestor(root.right, 7,4);
+                                                                                              if(left==null){
+                                                                                              return right;
+                                                                                             }else if(right==null){
+                                                                                              return left;
+                                                                                             }else
+                                                                                           return root;
+                                                                                              }  
+                                                                                              
+        lowestCommonAncestor(root(6),7, 4) {
+        if(root==null || root==7||root==4){
+            return root;
+        }
+        TreeNode left=lowestCommonAncestor(root.left (null), 7,4);--------->
+        TreeNode right=lowestCommonAncestor(root.right, 7,4);
+        if(left==null){
+            return right;
+        }else if(right==null){
+            return left;
+        }else
+        return root;
+    }                                                                                                 
+                                                                                             lowestCommonAncestor(root(null),7, 4) {
+                                                                                              if(root==null || root==7||root==4){  //this part execute 
+                                                                                               return root; // return null
+                                                                                               }
+                                                                                             TreeNode left=lowestCommonAncestor(root.left (6), 7,4); // left=null
+                                                                                              TreeNode right=lowestCommonAncestor(root.right, 7,4); // also null 
+                                                                                              if(left==null){
+                                                                                              return right; // this execute return null
+                                                                                             }else if(right==null){
+                                                                                              return left;
+                                                                                             }else
+                                                                                           return root;
+                                                                                              }                                                                                         
+                                                                                              
+                                                                                              
+                                                                                              
+                                                                                              
+                                                                                              
+                         
+*/                            
