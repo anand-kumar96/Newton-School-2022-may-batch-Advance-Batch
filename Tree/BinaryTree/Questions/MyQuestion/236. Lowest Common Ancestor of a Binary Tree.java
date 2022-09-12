2744fa948fb35a89bad4af5df994e3b2386bf2db
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-09-12 13:26:35 +0530

    Update 236. Lowest Common Ancestor of a Binary Tree.java

--- Tree/BinaryTree/Questions/MyQuestion/236. Lowest Common Ancestor of a Binary Tree.java
+++ Tree/BinaryTree/Questions/MyQuestion/236. Lowest Common Ancestor of a Binary Tree.java
@@ -23,6 +23,57 @@ class Solution {
         return root;
     }
 }
+// method 02
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+       if(root==null || root==p || root==q){
+           return root;
+       } 
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left==null && right==null){
+           return null; 
+        }else if(left!=null && right!=null){
+            return root;
+        }else if(left==null){
+            return right;
+        }else
+            return left;
+    }
+}
+
+//// method 03
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+       if(root==null){
+           return root;
+       }
+         if(root.val==p.val || root.val==q.val){
+            return root;
+        }
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left==null && right==null){
+           return null; 
+        }else if(left!=null && right!=null){
+            return root;
+        }else if(left==null){
+            return right;
+        }else
+            return left;
+    }
+}
 /*
 explanation:
 3,5,1,6,2,0,8,null,null,7,4     case (1) p=7, q =4 
