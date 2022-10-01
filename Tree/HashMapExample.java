278fbb32909c3d54607e673618eee6f54fc5162f
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-02 00:52:09 +0530

    Create HashMapExample.java

---
+++ Tree/HashMapExample.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        Map<Integer, Integer> rollNumberMarksMap = new HashMap<>();
+
+        rollNumberMarksMap.put(1, 84);
+        rollNumberMarksMap.put(2, 91);
+        rollNumberMarksMap.put(3, 90);
+        rollNumberMarksMap.put(4, 22);
+        rollNumberMarksMap.put(5, 67);
+        rollNumberMarksMap.put(6, 78);
+        rollNumberMarksMap.put(9, 51);
+        rollNumberMarksMap.put(41, 16);
+        rollNumberMarksMap.put(10, 100);
+        rollNumberMarksMap.put(11, 100);
+        rollNumberMarksMap.put(12, 100);
+        rollNumberMarksMap.put(13, 100);
+        rollNumberMarksMap.put(14, 100);
+        rollNumberMarksMap.put(15, 100);
+    }
+}
