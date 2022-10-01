3a95344fa2c2edebb8db46d8d9a5d308970e9b5a
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-01 23:05:54 +0530

    Update Foldable or Not.java

--- Tree/BinaryTree/Questions/Assignment/Binary Tree/Binary Tree - In Class/Foldable or Not.java
+++ Tree/BinaryTree/Questions/Assignment/Binary Tree/Binary Tree - In Class/Foldable or Not.java
@@ -1,3 +1,5 @@
+
+// method 01
 /*
 // Information about the class Node
 class Node{
@@ -28,6 +30,37 @@ static boolean checkNode(Node left,Node right ){
     return checkNode(left.left, right.right)
             && checkNode(left.right, right.left);
 }
+
+
+// method 02
+/*
+// Information about the class Node
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+public static boolean check(Node node1,Node node2){
+    if(node1==null && node2==null)
+     return true;
+    if(node1==null || node2==null)
+        return false;
+    return check(node1.left, node2.right)
+    && check(node1.right, node2.left);
+}
+static boolean isFoldable(Node node){
+ if(node==null || (node.left==null && node.right==null)) {
+     return true;
+ }
+return check(node.left,node.right);
+}
+
 
 /*
 Foldable or Not
