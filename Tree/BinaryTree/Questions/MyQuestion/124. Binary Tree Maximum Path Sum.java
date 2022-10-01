52df8dc470543f29c8b1fff92c070bef8b7e35f8
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-01 13:38:34 +0530

    Update 124. Binary Tree Maximum Path Sum.java

--- Tree/BinaryTree/Questions/MyQuestion/124. Binary Tree Maximum Path Sum.java
+++ Tree/BinaryTree/Questions/MyQuestion/124. Binary Tree Maximum Path Sum.java
@@ -14,21 +14,51 @@
  *     }
  * }
  */
-// extension of diameter
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int findmaxsum(TreeNode root,int maxi[]){
+     // extension of diameter
+    public int findMaxPath(TreeNode root,int maxi[]){
         if(root==null){
             return 0;
         }
-        int lheight=Math.max(0,findmaxsum(root.left,maxi));
-        int rheight=Math.max(0,findmaxsum(root.right,maxi));
-        maxi[0]=Math.max(maxi[0],root.val+lheight+rheight);
-        return root.val+(Math.max(lheight,rheight));
+            // int leftSum=findMaxPath(root.left,maxi);
+            // int rightSum=findMaxPath(root.right,maxi);
+        
+        
+          // to ignore -ve of left an d right part we have to right 
+         int leftSum=Math.max(0,findMaxPath(root.left,maxi));
+        int rightSum=Math.max(0,findMaxPath(root.right,maxi));
+        
+            maxi[0]=Math.max(maxi[0],(root.val+leftSum+rightSum));
+            return root.val+Math.max(leftSum,rightSum);
+      
+        
     }
     public int maxPathSum(TreeNode root) {
-        int maxi[]=new int [1];
+        int maxi[]=new int[1];
         maxi[0]=Integer.MIN_VALUE;
-        findmaxsum(root,maxi);
+        int ans=findMaxPath(root,maxi);
         return maxi[0];
     }
 }
+
+// ex
+/*
+                10 
+            15      30
+                -10    -8
+*/
