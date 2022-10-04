cef0dc3936a66a57360cc276957e74b56f47b6e7
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-04 23:01:35 +0530

    Update 98. Validate Binary Search Tree.java

--- Tree/BinarySearchTree/Question/Leetcode/98. Validate Binary Search Tree.java
+++ Tree/BinarySearchTree/Question/Leetcode/98. Validate Binary Search Tree.java
@@ -119,3 +119,29 @@ class Solution {
         return result;
     }
 }
+
+//
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode prev = null;
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        
+        if(isValidBST(root.left) && (prev == null || root.val > prev.val)) {
+            prev = root;
+            return isValidBST(root.right);
+        }
+        
+        return false;
+    }
+}
