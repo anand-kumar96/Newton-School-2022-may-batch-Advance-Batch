2c5f4ccbabfccd48f6acc62d44aa28b536e5b98e
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-04 13:29:15 +0530

    Update Second maximum node .java

--- Tree/BinaryTree/Questions/Assignment/Binary Tree/Binary Tree - In Class 3/Second maximum node .java
+++ Tree/BinaryTree/Questions/Assignment/Binary Tree/Binary Tree - In Class 3/Second maximum node .java
@@ -1,4 +1,52 @@
 // method 01
+
+//class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+//     TreeNode() {}
+//     TreeNode(int val) { this.val = val; }
+//     TreeNode(int val, TreeNode left, TreeNode right) {
+//         this.val = val;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
+
+class Solution {
+   static long arr[];
+   static int i;
+   public static void findsecondmax(TreeNode root){
+    if(root==null){
+        return;
+    }
+      arr[i++]=root.val;
+      findsecondmax(root.left);
+      findsecondmax(root.right);
+   }
+    public int SecondMaxNode(TreeNode root){
+        i=0;
+        arr=new long [1000001];
+        if(root==null){
+        return -1;
+    }
+     if(root.left==null && root.right==null){
+        return root.val;
+    }
+    findsecondmax(root);
+    int j=i;
+    int k=0;
+    long arr1[]=new long [j];
+    for(int i=0;i<j;i++){
+        arr1[k++]=arr[i];
+    }
+    Arrays.sort(arr1);
+       return (int)arr1[j-2];
+    }
+
+
+
+// method 02
 //class TreeNode {
 //     int val;
 //     TreeNode left;
@@ -43,7 +91,7 @@ class Solution {
     }
 }
 
-// method 02 
+// method 03
 
 //class TreeNode {
 //     int val;
