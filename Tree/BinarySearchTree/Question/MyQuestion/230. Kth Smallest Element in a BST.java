403f0046fbff649791255bc487622c4b7327784b
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-09 00:51:24 +0530

    Create 230. Kth Smallest Element in a BST.java

---
+++ Tree/BinarySearchTree/Question/MyQuestion/230. Kth Smallest Element in a BST.java
@@ -0,0 +1,85 @@
+//https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+// my method
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static TreeSet<Integer>hs;
+    public void kthSmallestHelper(TreeNode currentNode){
+        if(currentNode!=null){
+            if(!hs.contains(currentNode.val)){
+                hs.add(currentNode.val);
+            }
+            kthSmallestHelper(currentNode.left);
+            kthSmallestHelper(currentNode.right);
+        }
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        hs=new TreeSet<>();
+        kthSmallestHelper(root);
+        int result=0;
+        int ans=-1;
+       for(Integer value:hs){
+           result++;
+           if(result==k){
+               ans=value;
+               break;
+           }
+       }
+        return ans;
+    }
+}
+
+// method 01
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static int result;
+    static int count;
+    public void kthSmallestHelper(TreeNode root, int k){
+         if(root!=null){
+        kthSmallestHelper(root.left, k);
+             count++;
+         if(count==k){
+             result=root.val;
+                return;
+         }
+          kthSmallestHelper(root.right, k);
+        }
+    }
+    public int kthSmallest(TreeNode root, int k) {
+       result=-1;
+        count=0;
+        kthSmallestHelper(root,k);
+    
+        return result;
+    }
+}
