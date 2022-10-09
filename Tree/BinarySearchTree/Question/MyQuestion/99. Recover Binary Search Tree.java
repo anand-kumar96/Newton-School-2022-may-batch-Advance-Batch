1ecfffce24ea4589ebfd02a5c08b95b7ae204d96
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-09 21:56:58 +0530

    Create 99. Recover Binary Search Tree.java

---
+++ Tree/BinarySearchTree/Question/MyQuestion/99. Recover Binary Search Tree.java
@@ -0,0 +1,47 @@
+//explanation:https://leetcode.com/problems/recover-binary-search-tree/discuss/32562/Share-my-solutions-and-detailed-explanation-with-recursiveiterative-in-order-traversal-and-Morris-traversal
+// method. 01
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static List<Integer>li;
+    public void Inorder(TreeNode currentNode){
+        if( currentNode!=null){
+            Inorder(currentNode.left);
+            li.add(currentNode.val);
+            Inorder(currentNode.right);
+        }
+    }
+    static int j;
+    public void InorderHelper(TreeNode currentNode){
+         if( currentNode!=null){
+            InorderHelper(currentNode.left);
+             if(currentNode.val!=li.get(j)){
+                currentNode.val=li.get(j); 
+                 j++;
+             }else{
+                 j++;
+             }
+            InorderHelper(currentNode.right);
+        }
+    }
+    public void recoverTree(TreeNode root) {
+        li=new ArrayList<>();
+        Inorder(root);
+       Collections.sort(li);
+        j=0;
+        InorderHelper(root);
+    }
+}
