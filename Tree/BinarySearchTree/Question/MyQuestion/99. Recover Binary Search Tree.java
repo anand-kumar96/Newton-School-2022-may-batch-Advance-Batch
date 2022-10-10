63699863846ba8769888ee89287f90408e677a91
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-10 15:16:13 +0530

    Update 99. Recover Binary Search Tree.java

--- Tree/BinarySearchTree/Question/MyQuestion/99. Recover Binary Search Tree.java
+++ Tree/BinarySearchTree/Question/MyQuestion/99. Recover Binary Search Tree.java
@@ -1,5 +1,9 @@
 //explanation:https://leetcode.com/problems/recover-binary-search-tree/discuss/32562/Share-my-solutions-and-detailed-explanation-with-recursiveiterative-in-order-traversal-and-Morris-traversal
+//https://www.youtube.com/watch?v=ZWGW7FminDM
+
+
 // method. 01
+// // tc=2*O(N)+Nlog(N), Ts=O(N)
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -46,4 +50,57 @@ class Solution {
     }
 }
 // method. 02
-
+// tc=O(N), Ts=O(1)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     TreeNode first;
+     TreeNode second;
+     TreeNode last;
+     TreeNode prev;
+    public void InorderHelper(TreeNode currentNode){
+         if( currentNode!=null){
+            InorderHelper(currentNode.left);
+             if(prev!=null && currentNode.val<prev.val){
+                 if(first==null){
+                     first=prev;
+                     second=currentNode;
+                 }else{
+                     last=currentNode;
+                 }
+             }
+              prev=currentNode;
+            InorderHelper(currentNode.right);
+        }
+    }
+    public void recoverTree(TreeNode root) {
+             first=null;
+             second=null;
+             prev=new TreeNode( Integer.MIN_VALUE);
+             last=null;
+            InorderHelper(root);
+            if(first!=null && last!=null){
+                int temp=first.val;
+                first.val=last.val;
+                last.val=temp;
+            }
+        if(first!=null && last==null){
+            int temp=first.val;
+                first.val=second.val;
+                second.val=temp;
+        }
+    }
+}
