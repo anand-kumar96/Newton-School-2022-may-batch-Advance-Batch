f842a4787dbe8026b457eee327331ed8f5433f07
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-10 15:46:11 +0530

    Update 99. Recover Binary Search Tree.java

--- Tree/BinarySearchTree/Question/MyQuestion/99. Recover Binary Search Tree.java
+++ Tree/BinarySearchTree/Question/MyQuestion/99. Recover Binary Search Tree.java
@@ -104,3 +104,48 @@ class Solution {
         }
     }
 }
+// method 03
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+      TreeNode firstNode = null;
+    TreeNode secondNode = null;
+
+    TreeNode previousElement = new TreeNode(Integer.MIN_VALUE);
+
+    private void recoverTreeHelper(TreeNode currentNode) {
+        if(currentNode != null) {
+            recoverTreeHelper(currentNode.left);
+            //process
+            if(firstNode == null && previousElement.val > currentNode.val) {
+                firstNode = previousElement;
+            }
+            if(firstNode != null && previousElement.val > currentNode.val) {
+                secondNode = currentNode;
+            }
+            previousElement = currentNode;
+
+            recoverTreeHelper(currentNode.right);
+        }
+    }
+    public void recoverTree(TreeNode root) {
+        recoverTreeHelper(root);
+        //swap
+        int temp = firstNode.val;
+        firstNode.val = secondNode.val;
+        secondNode.val = temp;
+    }
+}
