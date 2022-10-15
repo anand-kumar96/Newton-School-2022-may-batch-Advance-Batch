6383bfee8de518659c3123aebcd6974e994c6f68
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-15 15:56:37 +0530

    Update Difference in operations.java

--- Tree/BinarySearchTree/Question/Assignment/Binary Search Tree - In Class-2/Difference in operations.java
+++ Tree/BinarySearchTree/Question/Assignment/Binary Search Tree - In Class-2/Difference in operations.java
@@ -88,3 +88,25 @@ class Main {
     System.out.println((min*max)-(min+max));
     }
 }
+ /*
+ Difference in operations
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Given the root of a binary search tree. Calculate the difference between the product and sum of minimum and maximum elements of the binary search tree.
+Input
+Given root of binary search tree.
+
+Constraints:
+1<= node value<=100000
+Output
+print difference between product of minimum and maximum element of BST.
+Example
+Sample Input 1:
+root = 4 2 6 1 3
+
+Sample Output 1:
+-1
+Explanation: Product of maximum and minimum element is 6 and their sum is 7 so difference between these two is -1.
+ */
