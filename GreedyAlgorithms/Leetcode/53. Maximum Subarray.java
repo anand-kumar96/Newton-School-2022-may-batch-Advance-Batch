08d60b295c16299323079d07aed2531b24379cc6
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-28 09:49:56 +0530

    Update 53. Maximum Subarray.java

--- GreedyAlgorithms/Leetcode/53. Maximum Subarray.java
+++ GreedyAlgorithms/Leetcode/53. Maximum Subarray.java
@@ -38,3 +38,23 @@ class Solution {
         return maxsum;
     }
 }
+// method 02
+class Solution {
+   public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int maxElement = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if(sum < 0) {
+                sum = 0;
+            }
+            maxSum = Math.max(maxSum, sum);
+            maxElement = Math.max(maxElement, nums[i]);
+        }
+        if(maxSum == 0) {
+            maxSum = maxElement;
+        }
+        return maxSum;
+    }
+}
