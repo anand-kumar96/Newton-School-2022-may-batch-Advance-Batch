712464bf6226bcb98ed7c0196d0e3c6a9942c198
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-28 08:57:42 +0530

    Create 122. Best Time to Buy and Sell Stock II.java

---
+++ GreedyAlgorithms/Leetcode/122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+//method 01
+class Solution {
+    public int maxProfit(int[] prices) {
+      int profit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                profit+=prices[i]-prices[i-1];
+                }
+        }
+        return profit;
+    }
+}
