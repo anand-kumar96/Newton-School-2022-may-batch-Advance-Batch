c7e4bb4aa951b6d26920c839304b331dd55a3472
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-30 01:47:00 +0530

    Create 45. Jump Game II.java

---
+++ GreedyAlgorithms/Leetcode/45. Jump Game II.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/jump-game-ii/
+//method 01
+class Solution {
+    public int jump(int[] nums) {
+        int reachingPosition=0;// reaching or maxdistance in a particular position
+        int max=0;
+        int count=0;// minimum step to reach destination
+        for(int i=0;i<nums.length-1;i++){
+            max=Math.max(max,nums[i]+i);
+            if(i==reachingPosition){
+                count++;
+                reachingPosition=max;
+                max=0;
+            }
+        }
+        return count;
+    }
+}
+
+// method 02
