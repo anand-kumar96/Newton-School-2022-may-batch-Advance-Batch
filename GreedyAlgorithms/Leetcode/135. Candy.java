ef6a635191e3080cedc3ab3d1fb6b091ac689adc
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-30 03:13:45 +0530

    Update 135. Candy.java

--- GreedyAlgorithms/Leetcode/135. Candy.java
+++ GreedyAlgorithms/Leetcode/135. Candy.java
@@ -28,3 +28,30 @@ class Solution {
         return totalCandy;
     }
 }
+// method 02--> optimized
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int arrLR[]=new int[n];
+        int arrRL[]=new int[n];
+        Arrays.fill(arrLR,1);
+        Arrays.fill(arrRL,1);
+
+       for(int i=1;i<n;i++){
+          // from left to right
+          if(ratings[i]>ratings[i-1]){
+              arrLR[i]=arrLR[i-1]+1;
+          }
+          // from right  to left
+          if(ratings[n-1-i]>ratings[n-i]){
+              arrRL[n-1-i]=arrRL[n-i]+1;
+          }
+      }
+        int totalCandy=0;
+        // taking max between both
+        for(int i=0;i<n;i++){
+            totalCandy+=Math.max(arrLR[i],arrRL[i]);
+        }
+        return totalCandy;
+    }
+}
