ae4c4cab07fb1814f5b5e98daf085ad3a4769b19
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-10-30 03:09:08 +0530

    Create 135. Candy.java

---
+++ GreedyAlgorithms/Leetcode/135. Candy.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/candy/
+// method 01
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int arrLR[]=new int[n];
+        int arrRL[]=new int[n];
+        Arrays.fill(arrLR,1);
+        Arrays.fill(arrRL,1);
+        // from left to right
+      for(int i=1;i<n;i++){
+          if(ratings[i]>ratings[i-1]){
+              arrLR[i]=arrLR[i-1]+1;
+          }
+      }
+        // from right  to left
+        for(int i=n-2;i>=0;i--){
+          if(ratings[i]>ratings[i+1]){
+              arrRL[i]=arrRL[i+1]+1;
+          }
+      }
+        int totalCandy=0;
+        for(int i=0;i<n;i++){
+            totalCandy+=Math.max(arrLR[i],arrRL[i]);
+        }
+        return totalCandy;
+    }
+}
