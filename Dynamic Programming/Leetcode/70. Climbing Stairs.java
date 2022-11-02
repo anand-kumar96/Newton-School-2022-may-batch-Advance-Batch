b69fb0194d926fd79a8b31cc3e51259b06735fcc
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-11-02 10:57:08 +0530

    Update 70. Climbing Stairs.java

--- Dynamic Programming/Leetcode/70. Climbing Stairs.java
+++ Dynamic Programming/Leetcode/70. Climbing Stairs.java
@@ -20,3 +20,20 @@ class Solution {
         return arr[n];
     }
 }
+
+// method 02
+class Solution {
+    // without using array
+    public int climbStairs(int n) {
+        if(n==1 || n==2) return n;
+        int first=1;
+        int second=2;
+        int third=0;
+        for(int i=3;i<=n;i++){
+           third=first+second;
+            first=second;
+            second=third;
+        }
+        return third;
+    }
+}
